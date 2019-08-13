af10fd1721e813d52cd60de40b515b0f01e2aa8f
dearCorazon <30954337+dearCorazon@users.noreply.github.com>
2019-08-13 11:06:22 +0800

    first

---
+++ titanic/main.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+
+data_train = pd.read_csv(r"C:\Users\inf\Documents\Master\Titanic\titanic\train.csv")
+#data_train.info()
+data_train.describe()
+
